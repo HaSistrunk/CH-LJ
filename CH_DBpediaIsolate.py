a34fb2840ae415a3d1012fd7f0115318dbc6406f
HaSistrunk <hsistrunk@gmail.com>
2015-11-01 18:24:30 -0500

    Create json file of CH dbpedia URIs

---
+++ CH_DBpediaIsolate.py
@@ -0,0 +1,38 @@
+
+#This script creates a json file from a list of all CH dbpedia URIs
+
+
+from rdflib import Graph
+import re
+import json
+
+
+#create data graph for CH triples
+ch = Graph()
+
+#Parse source, adding the resulting triples to the Graph.
+#The file source is specified with a parameter added for format
+ch.parse("ch_lj.nt", format="nt")
+
+
+#create list of all triples in ch
+chlist = []
+
+#loop through, find 'dbpedia' in object (not all objects are names so I am foregoing counting all of them)
+for s,p,o in ch:
+
+    if 'dbpedia' in o:
+        if o not in chlist:
+            #build list from objects because subject repeats
+            chlist.append(o)
+
+    # print(len(ch))
+
+# #convert to string
+chstring = ' '.join(chlist)
+
+# print (chstring)
+
+#write out json file
+with open ('CH_dbpedia.json', 'w') as f:
+    f.write(json.dumps(chlist)

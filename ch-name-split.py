27af93de8d9eee51cdca53928bf69d8f798e3b56
mreesele <mollyreeselerner@gmail.com>
2015-11-02 15:50:09 -0500

    Splits CH names on the _ : is this superfluous?

---
+++ ch-name-split.py
@@ -0,0 +1,12 @@
+import re
+import json
+
+#open json file and load
+with open('CH_namesOnly.json') as json_data:
+    d = json.load(json_data)
+    json_data.close()
+
+for a_name in d:
+    names = a_name.split("_")
+
+    print(names)

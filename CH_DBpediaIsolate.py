42e49523d0775e5cbe44a513c1b879943cb00bd5
mreesele <mollyreeselerner@gmail.com>
2015-11-02 09:57:48 -0500

    Update CH_DBpediaIsolate.py
    Add 'else' statement to route CH mints to another json file.

--- CH_DBpediaIsolate.py
+++ CH_DBpediaIsolate.py
@@ -16,6 +16,7 @@ ch.parse("ch_linkedJazz.nt", format="nt")
 
 #create list of all triples in ch
 chlist = []
+mintsCH = []
 
 #loop through, find 'dbpedia' in object (not all objects are names so I am foregoing counting all of them)
 for s,p,o in ch:
@@ -24,14 +25,25 @@ for s,p,o in ch:
         if o not in chlist:
             #build list from objects because subject repeats
             chlist.append(o)
+    
+    else:
+        if "data.carnegiehall" in o:
+            if o not in mintsCH:
+                mintsCH.append(o)
 
     # print(len(ch))
 
 # #convert to string
 chstring = ' '.join(chlist)
+mintstring = ' '.join(mintsCH)
 
 # print (chstring)
 
 #write out json file
 with open ('CH_dbpedia.json', 'w') as f:
     f.write(json.dumps(chlist ,indent=4))
+    
+with open ('CH_mints.json', 'w') as f:
+    f.write(json.dumps(mintsCH ,indent=4))
+    
+    
